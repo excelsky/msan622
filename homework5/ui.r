070413108f3ffeb6611e504e3559acf30f471690
excelsky <analytics20132333@gmail.com>
2014-04-26 00:00:14 -0700

    Update ui.r

--- homework5/ui.r
+++ homework5/ui.r
@@ -1,48 +1,78 @@
 shinyUI(pageWithSidebar(
-    headerPanel("UK Deaths from Lung Diseases"),
-    
-    sidebarPanel(
-        sliderInput(
-            "num", 
-            "Months:", 
-            min = 4, 
-            max = 24,
-            value = 12, 
-            step = 1
-        ),
-        
-        sliderInput(
-            "start", 
-            "Starting Point:",
-            min = 1974, 
-            max = 1980,
-            value = 1974, 
-            step = 1 / 12,
-            round = FALSE, 
-            ticks = TRUE,
-            format = "####.##",
-            animate = animationOptions(
-                interval = 800, 
-                loop = TRUE
-            )
-        ),
-        
-        width = 3
+  headerPanel("Road Casualties in Great Britain 1969 - 1984"),
+  
+  sidebarPanel(
+    sliderInput(
+      "num", 
+      "Months:", 
+      min = 4, 
+      max = 24,
+      value = 12, 
+      step = 1
     ),
     
-    mainPanel(
-        plotOutput(
-            outputId = "mainPlot", 
-            width = "100%", 
-            height = "400px"
-        ),
-        
-        plotOutput(
-            outputId = "overviewPlot",
-            width = "100%",
-            height = "200px"
-        ),
-        
-        width = 9
-    )
+    # Add a little bit of space between widgets.
+    br(),
+    
+    sliderInput(
+      "start", 
+      "Starting Point:",
+      min = 1969, 
+      max = 1984,
+      value = 1969, 
+      step = 1 / 12,
+      round = FALSE, 
+      ticks = TRUE,
+      format = "####.##",
+      animate = animationOptions(
+        interval = 800, 
+        loop = TRUE
+      )
+    ),
+    
+    # Add a little bit of space between widgets.
+    br(),
+    # Add a little bit of space between widgets.
+    br(),
+    
+    # Checkbox Group Input Control
+    checkboxGroupInput("SAPvar", "Variables for Stacked Area Plot:\n
+                       (I wish I could force the color consistent.)", 
+                       c("DriversKilled",
+                         "front",
+                         "rear")
+                       , selected = c("DriversKilled",
+                                      "front",
+                                      "rear")
+    ),  # checkboxGroupInput
+    
+    # Add a little bit of space between widgets.
+    br(),
+    
+    # Create radio buttons.
+    radioButtons("LPvar", "Variables for Line Plot:",
+                 c("DriversKilled",
+                   "front",
+                   "rear"),
+                 selected = "DriversKilled"
+    ),  # radioButtons
+    
+    width = 3
+  ),
+  
+  mainPanel(
+    plotOutput(
+      outputId = "mainPlot", 
+      width = "100%", 
+      height = "400px"
+    ),
+    
+    plotOutput(
+      outputId = "overviewPlot",
+      width = "100%",
+      height = "200px"
+    ),
+    
+    width = 9
+  )
 ))
