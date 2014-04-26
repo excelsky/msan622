4c9ec7e1dc12e84a8d290813e5052d06e5a55821
excelsky <analytics20132333@gmail.com>
2014-04-25 23:59:53 -0700

    Update server.r

--- homework5/server.r
+++ homework5/server.r
@@ -1,10 +1,10 @@
 
 shinyServer(function(input, output) {
     output$mainPlot <- renderPlot({
-        print(plotArea(input$start, input$num))
+        print(plotArea(input$start, input$num, input$SAPvar))
     })
     
     output$overviewPlot <- renderPlot({
-        print(plotOverview(input$start, input$num))
+        print(plotOverview(input$start, input$num, input$LPvar))
     })
 })
